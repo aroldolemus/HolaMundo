a68483f7fe4324a75fe91f308f08dfedc8f32444
aroldolemus <42223314+aroldolemus@users.noreply.github.com>
2018-08-08 17:03:06 -0600

    Add files via upload

---
+++ HolaMundoAutomatas.java
@@ -0,0 +1,17 @@
+
+package holamundoautomatas;
+
+// @author FRANKLIN IT
+
+public class HolaMundoAutomatas {
+    
+    public static void main(String[] args) {
+       System.out.println("FRANKLIN AROLDO LÉMUS JIMÉNEZ");
+       System.out.println("Carné: 1390-06-6591");
+       System.out.println("AUTOMATAS Y LENGUAJES FORMALES");
+       System.out.println("********************");
+       System.out.println("HOLA MUNDO!!!");
+       System.out.println("********************");
+    }
+    
+}
